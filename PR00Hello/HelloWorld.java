ff466d835b4a00a40b15ffd921af6952c80036e6
Enrico <envomp@ttu.ee>
2019-01-29 06:59:41 +0200

    hello

--- PR00Hello/HelloWorld.java
+++ PR00Hello/HelloWorld.java
@@ -2,6 +2,10 @@ package ee.taltech.iti0202.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        HelloWorld();
+    }
+
+    private static void HelloWorld(){
         System.out.println("Hello world!");
     }
 }
