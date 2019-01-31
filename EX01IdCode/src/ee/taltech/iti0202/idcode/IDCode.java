f1e7ba59909b97835d7ab34273493746b3fe2312
Enrico <envomp@ttu.ee>
2019-01-31 06:15:28 +0200

    ex01

--- EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
+++ EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
@@ -22,7 +22,8 @@ public class IDCode {
     public static boolean isIDCodeCorrect(String idCode) {
 
         return idCode.length() == ID_CODE_LENGTH && idCode.matches("[0-9]+")
-                && isGenderNumberCorrect(idCode) &&  isMonthNumberCorrect(idCode) && isDayNumberCorrect(idCode)
+                && isGenderNumberCorrect(idCode) && isYearNumberCorrect(idCode)
+                &&  isDayNumberCorrect(idCode)
                 && isQueueNumberCorrect(idCode) && isControlNumberCorrect(idCode);
 
     }
