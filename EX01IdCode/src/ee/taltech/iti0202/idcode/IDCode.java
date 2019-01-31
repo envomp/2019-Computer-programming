8ba169355866e8efdc9af0f1a9036b1f14ae5138
Enrico <envomp@ttu.ee>
2019-01-31 04:01:31 +0200

    ex01

--- EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
+++ EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
@@ -1,14 +1,29 @@
 package ee.taltech.iti0202.idcode;
 
 public class IDCode {
+
+    private static final int ID_CODE_LENGTH = 11;
+    private static final int MONTHS_IN_A_YEAR = 12;
+    private static final int DAYS_IN_LONG = 31;
+    private static final int DAYS_IN_SHORT = 30;
+    private static final int LEAP_MAX = 400;
+    private static final int EPOCH_YEAR = 1700;
+
+
+
     private enum Gender {
         MALE, FEMALE
     }
 
     public static boolean isIDCodeCorrect(String idCode) {
-        if (idCode.length() == 11 && idCode.matches("[0-9]+")) {
-            boolean[] correctness = {isGenderNumberCorrect(idCode), isYearNumberCorrect(idCode), isMonthNumberCorrect(idCode), isDayNumberCorrect(idCode), isQueueNumberCorrect(idCode), isControlNumberCorrect(idCode)};
-            for (boolean b : correctness) if (!b) return false;
+        if (idCode.length() == ID_CODE_LENGTH && idCode.matches("[0-9]+")) {
+            boolean[] correctness = {isGenderNumberCorrect(idCode), isYearNumberCorrect(idCode),
+                    isMonthNumberCorrect(idCode), isDayNumberCorrect(idCode),
+                    isQueueNumberCorrect(idCode), isControlNumberCorrect(idCode)};
+            for (boolean b : correctness)
+                if (!b) {
+                    return false;
+                }
             return true;
         }
         return false;
@@ -24,7 +39,7 @@ public class IDCode {
 
     private static boolean isMonthNumberCorrect(String idCode) {
         int month = Integer.parseInt(idCode.substring(3, 5));
-        return month > 0 && month < 13;
+        return month > 0 && month <= MONTHS_IN_A_YEAR;
     }
 
     private static boolean isDayNumberCorrect(String idCode) {
@@ -34,14 +49,14 @@ public class IDCode {
         int day = Integer.parseInt(idCode.substring(5, 7));
         if (Integer.parseInt(month) == 2) {
             if (leap) {
-                return day > 0 && day < 30;
+                return day > 0 && day <= DAYS_IN_LONG;
             } else {
-                return day > 0 && day < 29;
+                return day > 0 && day <= DAYS_IN_SHORT;
             }
         } else if (month.equals("04") || month.equals("06") || month.equals("09") || month.equals("11")) {
-            return day > 0 && day < 31;
+            return day > 0 && day <= DAYS_IN_SHORT;
         } else {
-            return day > 0 && day < 32;
+            return day > 0 && day <= DAYS_IN_LONG;
         }
 
     }
@@ -51,39 +66,41 @@ public class IDCode {
     }
 
     private static boolean isControlNumberCorrect(String idCode) {
-        int[] multipliers_1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1};
-        int[] multipliers_2 = {3, 4, 5, 6, 7, 8, 9, 1, 2, 3};
-        int control_number = 0;
+        int[] multipliers1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1};
+        int[] multipliers2 = {3, 4, 5, 6, 7, 8, 9, 1, 2, 3};
+        int controlNumber = 0;
         for (int i = 0; i < 10; i++) {
-            control_number += multipliers_1[i] * Character.getNumericValue(idCode.charAt(i));
+            controlNumber += multipliers1[i] * Character.getNumericValue(idCode.charAt(i));
         }
-        control_number %= 11;
-        if (control_number == 10) {
-            control_number = 0;
+        controlNumber %= 11;
+        if (controlNumber == 10) {
+            controlNumber = 0;
             for (int i = 0; i < 10; i++) {
-                control_number += multipliers_2[i] * idCode.charAt(i);
+                controlNumber += multipliers2[i] * idCode.charAt(i);
             }
-            control_number %= 11;
+            controlNumber %= 11;
         }
-        if (control_number == 10) {
-            control_number = 0;
+        if (controlNumber == 10) {
+            controlNumber = 0;
         }
-        return control_number == Integer.parseInt(idCode.substring(10));
+        return controlNumber == Integer.parseInt(idCode.substring(10));
     }
 
     private static boolean isLeapYear(int fullYear) {
-        if (fullYear % 400 == 0) {
+        if (fullYear % LEAP_MAX == 0) {
             return true;
         } else if (fullYear % 100 == 0) {
             return false;
-        } return fullYear % 4 == 0;
+        }
+        return fullYear % 4 == 0;
     }
 
     public static String getInformationFromIDCode(String idCode) {
         if (!isIDCodeCorrect(idCode)) {
             return "Given invalid ID code!";
         }
-        return "This is a " + getGender(idCode) + " born on " + idCode.substring(5, 7) + "." + idCode.substring(3, 5) + "." + getFullYear(idCode);
+        return "This is a " + getGender(idCode) + " born on " + idCode.substring(5, 7) +
+                "." + idCode.substring(3, 5) + "." + getFullYear(idCode);
     }
 
     public static Gender getGender(String idCode) {
@@ -95,10 +112,11 @@ public class IDCode {
     }
 
     public static int getFullYear(String idCode) {
-        return Integer.parseInt(idCode.substring(1, 3)) + 1700 + ((Integer.parseInt(idCode.substring(0, 1)) + 1) / 2) * 100;
+        return Integer.parseInt(idCode.substring(1, 3)) + EPOCH_YEAR +
+                ((Integer.parseInt(idCode.substring(0, 1)) + 1) / 2) * 100;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(isIDCodeCorrect("39907210831"));
     }
 }
