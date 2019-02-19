633181d307eae54bec5d2166c8b78242563c5864
Enrico <envomp@ttu.ee>
2019-02-19 03:52:58 +0200

    Pr04

--- PR04Stock/src/ee/taltech/iti0202/stock/product/Product.java
+++ PR04Stock/src/ee/taltech/iti0202/stock/product/Product.java
@@ -3,14 +3,15 @@ package ee.taltech.iti0202.stock.product;
 import ee.taltech.iti0202.stock.exceptions.StockException;
 
 public class Product {
-    static int count = 0;
+    public static int count = 0;
+    public int actual_count = 0;
     static int productNextId = 0;
     private String productName;
     private int productPrice;
 
     public Product(String name, int price) throws StockException {
         count += 1;
-
+        actual_count += 1;
         productName = name;
         productPrice = price;
     }
