9ad989abb5c36342af1b07f8cc45be14ff218869
Enrico <envomp@ttu.ee>
2019-02-22 00:33:35 +0200

    ex04

--- EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/CreditCard.java
+++ EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/CreditCard.java
@@ -34,7 +34,7 @@ public final class CreditCard extends BankCard {
         } else {
             throw new TransactionException();
         }
-        return getDebt();
+        return balance;
     }
 
     @Override
