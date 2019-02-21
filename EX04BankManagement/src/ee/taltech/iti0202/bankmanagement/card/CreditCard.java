6d50aaeafaf6d4b2f4e1e96f892ee8dd1e8e2ea3
Enrico <envomp@ttu.ee>
2019-02-22 00:46:16 +0200

    ex04

--- EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/CreditCard.java
+++ EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/CreditCard.java
@@ -27,8 +27,7 @@ public final class CreditCard extends BankCard {
 
     @Override
     public BigDecimal withdraw(BigDecimal value) throws TransactionException {
-        if (value.compareTo(BigDecimal.ZERO) >= 0
-                && balance.subtract(value).compareTo(BigDecimal.valueOf(-tenK / 2)) >= 0) {
+        if (balance.subtract(value).compareTo(BigDecimal.valueOf(-tenK / 2)) >= 0) {
             balance = balance.subtract(value);
         } else {
             throw new TransactionException();
