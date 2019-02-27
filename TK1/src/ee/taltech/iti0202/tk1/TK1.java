a3576a705164312e00424c1150f6707a46ccd46f
envomp <envomp@ttu.ee>
2019-02-27 10:39:06 +0200

    gj

---
+++ TK1/src/ee/taltech/iti0202/tk1/TK1.java
@@ -0,0 +1,67 @@
+package ee.taltech.iti0202.tk1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TK1 {
+    public static List<Integer> evenOdd(List<Integer> input) {
+        List<Integer> answer = new ArrayList<>();
+        for (Integer even : input){
+            if (even % 2 == 1){
+                answer.add(even);
+            }
+        }
+        for (Integer odd : input){
+            if (odd % 2 == 0){
+                answer.add(odd);
+            }
+        }
+
+        return answer;
+    }
+
+    public static int loneSum(int a, int b, int c) {
+        if(a == b || a == c || b == c){
+            return 0;
+        }
+        return a + b + c;
+    }
+
+    public static String getSandwich(String str) {
+        String[] answer = str.split("bread");
+        if (answer.length == 3){
+            return answer[1];
+        }
+        return "";
+    }
+
+    public static Map<String, String> topping(Map<String, String> map) {
+        if (map.containsKey("ice cream")){
+            map.put("ice cream", "cherry");
+        }
+        if (map.containsKey("bread")){
+            map.put("bread", "butter");
+        } else {
+            map.putIfAbsent("bread", "butter");
+        }
+
+        return  map;
+    }
+
+    public static void main(String[] args) {
+        // evenOdd([1, 0, 1, 0, 0, 1, 1]) // → [0, 0, 0, 1, 1, 1, 1]
+        // evenOdd([3, 3, 2]) // → [2, 3, 3]
+        // evenOdd([2, 2, 2]) // → [2, 2, 2]
+        // loneSum(1, 2, 3) // → 6
+        // loneSum(3, 2, 3) // → 2
+        // loneSum(3, 3, 3) // → 0
+        // getSandwich("breadjambread") // → "jam"
+        // getSandwich("xxbreadjambreadyy") // → "jam"
+        // getSandwich("xxbreadyy") // → ""
+        // topping({"ice cream": "peanuts"}) // → {"bread": "butter", "ice cream": "cherry"}
+        // topping({}) // → {"bread": "butter"}
+        // topping({"pancake": "syrup"}) // → {"bread": "butter", "pancake": "syrup"}
+    }
+}
