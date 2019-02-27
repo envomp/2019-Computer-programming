e8f387a55c6490cc025e3dd67db67637e6683119
envomp <envomp@ttu.ee>
2019-02-27 10:54:01 +0200

    gj

--- TK1/src/ee/taltech/iti0202/tk1/cat/Cat.java
+++ TK1/src/ee/taltech/iti0202/tk1/cat/Cat.java
@@ -1,4 +1,48 @@
 package ee.taltech.iti0202.tk1.cat;
 
 public class Cat {
+    private static String cname;
+    private static Integer cage;
+    private static String ccolor;
+
+    public Cat(String name,int age,String color){
+        cname = name;
+        cage = age;
+        ccolor = color;
+    }
+
+    public Cat(String name){
+        cname = name;
+        cage = null;
+        ccolor = null;
+    }
+
+    public String getName(){
+        return cname;
+    }
+
+    public Integer getAge(){
+        return cage;
+    }
+
+    public String getColor(){
+        return ccolor;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Mati");
+        System.out.println(cat);
+        Cat muri = new Cat("Muri", 3, "White");
+        System.out.println(muri);
+
+        Person malle = new Person();
+        Person kalle = new Person();
+        System.out.println(malle.addCat(cat)); // true
+        System.out.println(malle.addCat(cat)); // false
+
+        malle.sellCat(kalle, cat); // true
+        System.out.println(malle.getCats()); // []
+        System.out.println(kalle.getCats()); // [Mati]
+    }
 }
+
---
+++ TK1/src/ee/taltech/iti0202/tk1/cat/Person.java
@@ -0,0 +1,34 @@
+package ee.taltech.iti0202.tk1.cat;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private List<Cat> cats;
+    public Person(){
+        cats = new ArrayList<>();
+    }
+
+    public boolean addCat(Cat cat){
+        if (! cats.contains(cat)){
+            cats.add(cat);
+            return false;
+        }
+        return true;
+    }
+
+    public List<Cat> getCats() {
+        return cats;
+    }
+
+    public boolean sellCat(Person sellTo, Cat cat){
+        if (cats.contains(cat)) {
+            cats.remove(cat);
+            return sellTo.addCat(cat);
+        }
+        return false;
+    }
+
+}
