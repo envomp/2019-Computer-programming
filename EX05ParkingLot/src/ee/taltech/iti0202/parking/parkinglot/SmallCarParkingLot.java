655498d9fd547e3e7368fb69db2ecfb12b47670b
Enrico <envomp@ttu.ee>
2019-03-03 09:40:14 +0200

    hatred is encapsulating.. kek

--- EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/SmallCarParkingLot.java
+++ EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/SmallCarParkingLot.java
@@ -1,6 +1,8 @@
 package ee.taltech.iti0202.parking.parkinglot;
 
 
+import ee.taltech.iti0202.parking.car.Car;
+
 /**
  * This parking lot only accepts small cars (size 1).
  * Each parking slot only accepts one car.
@@ -18,7 +20,11 @@ public class SmallCarParkingLot extends ParkingLot {
 
     @Override
     public void processQueue() {
-        if (!getQueueCars().isEmpty()) queueToLot(getQueueCars().get(0), 2);
+        Car car = getQueueCars().get(0);
+        if (!getQueueCars().isEmpty() && this.getSpaceAvailable() > car.getSize()) {
+            queueToLot(car, 2);
+        }
+
         depark();
     }
 
