11eaf8c95983ed4661ee3abde22d526edaf06d93
Enrico <envomp@ttu.ee>
2019-03-05 22:42:35 +0200

    hatred is encapsulating.. kek

--- PR06Shelter/src/ee/taltech/iti0202/shelter/animalprovider/TestAnimalProvider.java
+++ PR06Shelter/src/ee/taltech/iti0202/shelter/animalprovider/TestAnimalProvider.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class TestAnimalProvider implements AnimalProvider {
 
     List<Animal> animals = new ArrayList<>();
-    List<Animal> animalBuffer = new ArrayList<>();
 
     public void addAnimal(Animal animal) {
         animals.add(animal);
@@ -17,14 +16,6 @@ public class TestAnimalProvider implements AnimalProvider {
     @Override
     public List<Animal> provide(Animal.Type type) {
 
-        System.out.println("Called");
-        if (animals.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        animalBuffer = List.of(animals.get(animals.size() - 1));
-        animals.remove(animals.size() - 1);
-
-        return animalBuffer;
+        return animals;
     }
 }
