e2a893f1656a150fb803954461c91b7f369fe2f5
Enrico <envomp@ttu.ee>
2019-03-06 19:46:32 +0200

    hatred is encapsulating.. kek

--- EX06Files/src/ee/taltech/iti0202/files/input/InputFilesScanner.java
+++ EX06Files/src/ee/taltech/iti0202/files/input/InputFilesScanner.java
@@ -2,25 +2,27 @@ package ee.taltech.iti0202.files.input;
 
 import ee.taltech.iti0202.files.exception.FileReaderException;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class InputFilesScanner implements InputFilesReader {
 
     @Override
     public List<String> readTextFromFile(String filename) throws FileReaderException {
 
-        ArrayList<String> fileContent = new ArrayList<>();
-
-        try (Scanner scanner = new Scanner(new File(filename))) {
-            while (scanner.hasNextLine()) {
-                fileContent.add(scanner.nextLine());
+        try {
+            BufferedReader scanner = new BufferedReader(new FileReader(filename));
+            ArrayList<String> fileContent = new ArrayList<>();
+            String cur_line;
+            while ((cur_line = scanner.readLine()) != null) {
+                fileContent.add(cur_line);
             }
+            return fileContent;
+
         } catch (Exception e) {
             throw new FileReaderException("No file was found!", e);
         }
-        return fileContent;
     }
 }
