0c23c5ff96440374683ccd7c4a8d9725f0064feb
Enrico <envomp@ttu.ee>
2019-03-06 23:22:47 +0200

    hatred is encapsulating.. kek

--- EX06Files/src/ee/taltech/iti0202/files/exception/FileReaderException.java
+++ EX06Files/src/ee/taltech/iti0202/files/exception/FileReaderException.java
@@ -2,20 +2,6 @@ package ee.taltech.iti0202.files.exception;
 
 public class FileReaderException extends RuntimeException {
 
-    /**
-     * Constructs a new runtime exception with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
-     * {@code cause} is <i>not</i> automatically incorporated in
-     * this runtime exception's detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     *                by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A {@code null} value is
-     *                permitted, and indicates that the cause is nonexistent or
-     *                unknown.)
-     * @since 1.4
-     */
     public FileReaderException(String message, Throwable cause) {
         super(message, cause);
     }
