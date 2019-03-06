253f46ea9b176aa984772a8a22014567d9101960
Enrico <envomp@ttu.ee>
2019-03-06 22:02:18 +0200

    hatred is encapsulating.. kek

--- EX06Files/src/ee/taltech/iti0202/files/exception/FileReaderException.java
+++ EX06Files/src/ee/taltech/iti0202/files/exception/FileReaderException.java
@@ -2,6 +2,6 @@ package ee.taltech.iti0202.files.exception;
 
 public class FileReaderException extends RuntimeException {
     public FileReaderException(Throwable e) {
-        super(e);
+        System.out.println(e);
     }
 }
