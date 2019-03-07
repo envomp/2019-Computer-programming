da47791f14f242f06533ed9c7fb56d8d131a3fe9
Enrico <envomp@ttu.ee>
2019-03-07 16:42:58 +0200

    hatred is encapsulating.. kek

--- EX06Files/src/ee/taltech/iti0202/files/MorseFilesController.java
+++ EX06Files/src/ee/taltech/iti0202/files/MorseFilesController.java
@@ -4,6 +4,6 @@ public class MorseFilesController {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello wkorld!");
+        System.out.println("Hello world!");
     }
 }
