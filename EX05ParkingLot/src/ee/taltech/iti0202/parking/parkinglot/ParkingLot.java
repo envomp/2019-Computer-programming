4a7cb8cfdffdd95f8e4785ca6e165c035959785d
Enrico <envomp@ttu.ee>
2019-03-08 21:49:42 +0200

    hatred is encapsulating.. kek

--- EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/ParkingLot.java
+++ EX05ParkingLot/src/ee/taltech/iti0202/parking/parkinglot/ParkingLot.java
@@ -23,7 +23,7 @@ import java.util.PriorityQueue;
  * <p>
  * Each concrete parking lot type (subclass)
  * has its own rules about which cars it accepts
- * in its queue and how the queue is processed.
+ * in its queue and how the queue is processedd.
  * See the class description for more information.
  */
 
