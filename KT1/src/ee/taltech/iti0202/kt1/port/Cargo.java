bba2c24a238ee206cbfe194bad6f1afcc4659398
Enrico Vompa <envomp@ttu.ee>
2019-04-03 11:19:32 +0300

    kt1 test2

--- KT1/src/ee/taltech/iti0202/kt1/port/Cargo.java
+++ KT1/src/ee/taltech/iti0202/kt1/port/Cargo.java
@@ -1,5 +1,36 @@
 package ee.taltech.iti0202.kt1.port;
 
 public class Cargo {
+    private String name;
+    private Integer weight;
+    private Integer size;
 
+    public Cargo(String name, int weight, int size) {
+        this.name = name;
+        this.weight = weight;
+        this.size = size;
+    }
+
+    public int getTotalWeight() {
+        if (weight < 10 || size < 10) {
+            return 10;
+        } return weight * size * size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, Total: %d", this.name, this.getTotalWeight());
+    }
 }
--- KT1/src/ee/taltech/iti0202/kt1/port/Port.java
+++ KT1/src/ee/taltech/iti0202/kt1/port/Port.java
@@ -1,4 +1,65 @@
 package ee.taltech.iti0202.kt1.port;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Port {
+    private String name;
+    private List<Ship> ships;
+    private List<Cargo> cargos;
+
+    public Port(String name, List<Ship> ships) {
+        this.name = name;
+        this.ships = ships;
+        this.cargos = new ArrayList<>();
+    }
+
+    public Port(String name) {
+        this.name = name;
+        this.ships = new ArrayList<>();
+        this.cargos = new ArrayList<>();
+    }
+
+    public List<Cargo> getCargoStorage() {
+        return cargos;
+
+    }
+
+    public void addShip(Ship ship) {
+        if (!ships.contains(ship)) {
+            ships.add(ship);
+        }
+    }
+
+    public void addCargo(List<Cargo> cargoList) {
+        for (Cargo cargo : cargoList) {
+            if (!cargos.contains(cargo)) {
+                cargos.add(cargo);
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ship> getShips() {
+        return ships;
+    }
+
+    public int emptyCargoStorage() {
+        int i = 0;
+        for (Cargo cargo : cargos) {
+            List<Ship> sortedShips = ships;
+            sortedShips.sort(Comparator.comparing(a -> a.getCargoList().size()));
+            for (Ship ship : sortedShips) {
+                if (ship.addCargo(cargo)) {
+                    i++;
+                    break;
+                }
+            }
+        }
+        return i;
+    }
 }
