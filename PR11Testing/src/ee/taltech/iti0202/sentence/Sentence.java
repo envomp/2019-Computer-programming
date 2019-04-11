b9a7e8e398950212d993a44d764caeebc0288728
Enrico <envomp@ttu.ee>
2019-04-11 08:51:33 +0300

    pr11

--- PR11Testing/src/ee/taltech/iti0202/sentence/Sentence.java
+++ PR11Testing/src/ee/taltech/iti0202/sentence/Sentence.java
@@ -1,9 +1,30 @@
 package ee.taltech.iti0202.sentence;
 
+import java.util.regex.Pattern;
+
 /**
  * Sentence class represent words and punctuation.
  */
 public class Sentence {
+    private String sentence;
+    private boolean hasCloser;
+
+    public Sentence(String tet) {
+        for (int i = 0; i < tet.length() - 1; i++) {
+            if ((tet.charAt(i) == '!' || tet.charAt(i) == '?' || tet.charAt(i) == '.') && tet.charAt(i + 1) == ' ') {
+                tet = tet.substring(0, i + 1);
+                break;
+            }
+        }
+        helper(tet);
+        if (sentence.endsWith("!") || sentence.endsWith("?") || sentence.endsWith(".")) {
+            hasCloser = true;
+        }
+    }
+
+    public Sentence() {
+        sentence = "";
+    }
 
     /**
      * Given string is treated as possible sentence.
@@ -12,13 +33,36 @@ public class Sentence {
      * If a word ends with ".", "!" or "?" treat it as punctuation.
      * No words can follow after punctuation - just ignore those.
      *
-     * @param tet Sentence as string
+     * @param text Sentence as string
      */
 
-    public Sentence(String tet) {
-    }
+    private void helper(String text) {
+        boolean lastWasSpace = false;
+        boolean startOfString = true;
+        StringBuilder noDupes = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
 
-    public Sentence() {
+            if (c == ' ') {
+                if (startOfString) {
+                    lastWasSpace = true;
+                } else if (!lastWasSpace) {
+                    lastWasSpace = true;
+                    noDupes.append(c);
+                }
+            } else {
+                lastWasSpace = false;
+                startOfString = false;
+                noDupes.append(c);
+            }
+        }
+        sentence = noDupes.toString();
+        if (!sentence.isEmpty()) {
+            sentence = sentence.substring(0, 1).toUpperCase() + sentence.substring(1);
+        }
+        while (sentence.endsWith(" ")) {
+            sentence = sentence.substring(0, sentence.length() - 1);
+        }
     }
 
     /**
@@ -31,7 +75,16 @@ public class Sentence {
      * @return Whether word was in the sentence and removed.
      */
     public boolean removeWord(String word) {
-        return true;
+        try {
+            if (!hasCloser) {
+                sentence = (Character.toLowerCase(sentence.charAt(0))
+                        + sentence.substring(1)).replaceFirst(Pattern.quote(word), "");
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return false;
     }
 
     /**
@@ -44,6 +97,14 @@ public class Sentence {
      * @return Whether word was added to sentence (false if sentence has punctuation).
      */
     public boolean addWord(String word) {
+        if (hasCloser) {
+            return false;
+        } else if (sentence.isEmpty()) {
+            sentence += word;
+        } else {
+            sentence += " " + word;
+        }
+
         return true;
     }
 
@@ -58,6 +119,11 @@ public class Sentence {
      * @return Whether punctuation was added (false if sentence already had punctuation).
      */
     public boolean addPunctuation(String punctuation) {
+        if (hasCloser || sentence.length() == 0) {
+            return false;
+        }
+        hasCloser = true;
+        sentence += punctuation;
         return true;
     }
 
@@ -71,18 +137,36 @@ public class Sentence {
      * @return Whether punctuation was removed (false if there was no punctuation).
      */
     public boolean removePunctuation() {
+        if (sentence.endsWith(".") || sentence.endsWith("!") || sentence.endsWith("?")) {
+            while (sentence.endsWith(".") || sentence.endsWith("!") || sentence.endsWith("?")) {
+                sentence = sentence.substring(0, sentence.length() - 1);
+            }
+            hasCloser = false;
+            return true;
+        }
         return false;
     }
 
     @Override
     public String toString() {
-        return "NO";
+        helper(sentence);
+        if (hasCloser || sentence.isEmpty()) {
+            return sentence;
+        }
+        return sentence + "...";
     }
 
 
     @Override
     public boolean equals(Object o) {
-        return true;
+
+        // null check
+        if (o == null) {
+            return false;
+        }
+
+        // this instance check
+        return this.toString().equals(o.toString());
     }
 
     @Override
