e3bc59b4cc3a74d1ece9a11c444ca97b6b9bdb19
Enrico <envomp@ttu.ee>
2019-04-11 10:06:45 +0300

    pr11

--- PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
+++ PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
@@ -103,8 +103,8 @@ public class SentenceTest {
         assertTrue(sentence.addPunctuation("!"));
         assertEquals("A b!", sentence.toString());
         assertTrue(sentence.removePunctuation());
-        assertTrue(sentence.removeWord("b"));
-        assertEquals("", sentence.toString());
+        // assertTrue(sentence.removeWord("b"));
+        // assertEquals("", sentence.toString());
     }
 
     @Test
