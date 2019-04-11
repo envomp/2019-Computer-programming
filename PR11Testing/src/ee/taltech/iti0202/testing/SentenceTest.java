53d32d1f11315fb35dc41c242385bb7e0101ae26
Enrico <envomp@ttu.ee>
2019-04-11 11:55:09 +0300

    pr11

--- PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
+++ PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
@@ -124,6 +124,8 @@ public class SentenceTest {
         assertEquals(s2, s2);
         assertEquals(s1, s2);
         assertEquals(s2, s1);
+        assertTrue(s1.equals(s2));
+        assertTrue(s2.equals(s1));
         assertEquals(s1.hashCode(), s2.hashCode());
         assertEquals(s2.hashCode(), s1.hashCode());
     }
