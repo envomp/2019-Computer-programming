4b2cc57bec07d8ffc684404984a794cc0f6f1293
Enrico <envomp@ttu.ee>
2019-04-13 01:24:14 +0300

    ex11

---
+++ EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/filter/WeightFilter.java
@@ -0,0 +1,20 @@
+package ee.taltech.iti0202.birdwatching.filter;
+
+import ee.taltech.iti0202.birdwatching.bird.Bird;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class WeightFilter implements BirdFilter {
+
+    private final double weight;
+
+    public WeightFilter(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public List<Bird> getSuitableBirds(List<Bird> birds) {
+        return birds.stream().filter(x -> x.getWeight() == weight).collect(Collectors.toList());
+    }
+}
---
+++ EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/filter/WingspanFilter.java
@@ -0,0 +1,25 @@
+package ee.taltech.iti0202.birdwatching.filter;
+
+import ee.taltech.iti0202.birdwatching.bird.Bird;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class WingspanFilter implements BirdFilter {
+
+    private final double minimum;
+    private final double maximum;
+
+    public WingspanFilter(double minimum, double maximum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    @Override
+    public List<Bird> getSuitableBirds(List<Bird> birds) {
+        return birds.stream()
+                .filter(x -> x.getWingspan() >= minimum)
+                .filter(y -> y.getWingspan() <= maximum)
+                .collect(Collectors.toList());
+    }
+}
