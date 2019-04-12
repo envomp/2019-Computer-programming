00c7ebcbf7263094b23a785852a77db83f44bcf5
Enrico <envomp@ttu.ee>
2019-04-13 01:36:59 +0300

    ex11

--- EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/filter/WeightFilter.java
+++ EX11Birdwatching/src/ee/taltech/iti0202/birdwatching/filter/WeightFilter.java
@@ -7,14 +7,19 @@ import java.util.stream.Collectors;
 
 public class WeightFilter implements BirdFilter {
 
-    private final double weight;
+    private final double minimum;
+    private final double maximum;
 
-    public WeightFilter(double weight) {
-        this.weight = weight;
+    public WeightFilter(double minimum, double maximum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
     }
 
     @Override
     public List<Bird> getSuitableBirds(List<Bird> birds) {
-        return birds.stream().filter(x -> x.getWeight() == weight).collect(Collectors.toList());
+        return birds.stream()
+                .filter(x -> x.getWeight() >= minimum)
+                .filter(y -> y.getWeight() <= maximum)
+                .collect(Collectors.toList());
     }
 }
