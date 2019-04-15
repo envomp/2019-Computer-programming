22caa3e9a7bc6925dfaa3b6c53cea25ca0df9f45
Enrico <envomp@ttu.ee>
2019-04-15 23:29:40 +0300

    ex11

--- PR12Recursion/src/ee/taltech/iti0202/recursion/Recursion.java
+++ PR12Recursion/src/ee/taltech/iti0202/recursion/Recursion.java
@@ -2,7 +2,6 @@ package ee.taltech.iti0202.recursion;
 
 public class Recursion {
 
-
     /**
      * Find the text between the first and last parenthesis.
      *
@@ -42,24 +41,5 @@ public class Recursion {
         if (!word.substring(0, 1).toLowerCase().matches("[ aeiouhklmnpwr'\"āōū.,!?]"))
             return pidginfy(word.substring(1));
         return word.charAt(0) + pidginfy(word.substring(1));
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println(parentheses("I am useless text(Find me), yet again useless")); // "(Find me)"
-        System.out.println(parentheses("This doesn't have any parentheses.")); // ""
-        System.out.println(parentheses("What do you do if (sentence has (many parentheses) and where it ends)")); // "(sentencce has (many parentheses) and where it ends)"
-        System.out.println();
-
-        System.out.println(removeDuplicates("aabbccddeeffgg")); // "abcdefg"
-        System.out.println(removeDuplicates("foakfjdirmdogmvooasf")); // "foakfjdirmdogmvoasf"
-        System.out.println(removeDuplicates("ilIliiiiilIili1lilllliiilil1ilili111111lili1")); // "ilIlilIili1lililil1ilili1lili1"
-        System.out.println();
-
-        System.out.println(pidginfy("Kūle'a ka'ōpopo'ōpiopio ma luna o ka'īlio palaualelo.")); // "Kūle'a ka'ōpopo'ōpiopio ma luna o ka'lio palaualelo."
-        System.out.println(pidginfy("kasmdfastu naidsfnasidn weraiskdfm sdfasdf''assdffaksndfasdf")); // "kamau nainain weraikm a''aakna"
-        System.out.println(pidginfy("He nani ka'iliahi, akā,'a'ohe mea'ala, no ka mea he mea'alala'i ka raiki, pono nō ka'ohe."));
-        //"He nani ka'iliahi, akā,'a'ohe mea'ala, no ka mea he mea'alala'i ka raiki, pono nō ka'ohe."
-
     }
 }
