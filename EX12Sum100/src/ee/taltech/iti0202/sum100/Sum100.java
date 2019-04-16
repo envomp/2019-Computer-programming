db3da1717d9a9d46ac6a528481f5a63571dfc674
Enrico <envomp@ttu.ee>
2019-04-16 11:55:34 +0300

    pr12

--- EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
+++ EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Sum100 {
 
-    public static List<String> calcSums() {
+    public static List<String> calcSums(List<String> numbers) {
         return List.of("100"); // TODO
     }
 }
