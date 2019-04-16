511c4cd8e91fea22e03a45145fb723fb2ee96bb4
Enrico <envomp@ttu.ee>
2019-04-17 02:15:15 +0300

    ex12

--- EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
+++ EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
@@ -1,6 +1,9 @@
 package ee.taltech.iti0202.sum100;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Sum100 {
 
