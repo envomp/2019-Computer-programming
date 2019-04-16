f33816bb32fac691dbbd0e66c0af043f62420672
Enrico <envomp@ttu.ee>
2019-04-17 00:16:53 +0300

    pr12

--- EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
+++ EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
@@ -1,10 +1,59 @@
 package ee.taltech.iti0202.sum100;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Sum100 {
 
-    public static List<String> calcSums(List<String> numbers) {
-        return List.of("100"); // TODO
+    public static List<String> calcSums() {
+        return possibilities(new char[]{' ', '-', '+'}, 9, "", new ArrayList<>());
+    }
+
+    public static List<String> possibilities(char[] c, int n, String start, List<String> answers) {
+        if (start.length() >= n) {
+            int i = 9;
+            List<Integer> cur_iteration = new ArrayList<>();
+            cur_iteration.add(0);
+            for (char operator : start.toCharArray()) {
+                switch (operator) {
+                    case ' ':
+                        int t = cur_iteration.remove(cur_iteration.size() - 1);
+                        if (t == 0)
+                            cur_iteration.add(i);
+                        else
+                            cur_iteration.add((int) (t + i * Math.pow(10, String.valueOf(t).length())));
+                        break;
+                    case '+':
+                        cur_iteration.add(i);
+                        break;
+                    case '-':
+                        cur_iteration.add(cur_iteration.remove(cur_iteration.size() - 1) * -1);
+                        cur_iteration.add(i);
+                        break;
+                }
+                i--;
+            }
+            if (cur_iteration.stream().mapToInt(Integer::intValue).sum() == 100) {
+                if (cur_iteration.get(0) == 0) cur_iteration.remove(0);
+                Collections.reverse(cur_iteration);
+                StringBuilder sb = new StringBuilder();
+                for (int elem : cur_iteration) {
+                    if (elem > 0 && !String.valueOf(elem).startsWith("1")) sb.append("+");
+                    sb.append(elem);
+                }
+                answers.add(sb.toString());
+            }
+        } else {
+            for (char x : c) {
+                possibilities(c, n, start + x, answers);
+            }
+        }
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calcSums());
     }
 }
