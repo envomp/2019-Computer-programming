44714ea1f14b8b84ac77600112fd5e63b0c0e7f9
Enrico <envomp@ttu.ee>
2019-04-17 00:51:18 +0300

    pr12

--- EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
+++ EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
@@ -1,51 +1,53 @@
 package ee.taltech.iti0202.sum100;
 
-
 import java.util.*;
 
 public class Sum100 {
 
+    public static final int numbersInList = 9;
+
     public static List<String> calcSums() {
-        List<String> possibilities = possibilities(new char[]{' ', '-', '+'}, 9, "", new ArrayList<>());
-        Set<String> set = new HashSet<>(possibilities);
-        possibilities.clear();
-        possibilities.addAll(set);
-        return possibilities;
+        return possibilities(new char[]{' ', '-', '+'}, numbersInList, "", new ArrayList<>());
     }
 
     public static List<String> possibilities(char[] c, int n, String start, List<String> answers) {
         if (start.length() >= n) {
-            int i = 9;
-            List<Integer> cur_iteration = new ArrayList<>();
-            cur_iteration.add(0);
+            int i = numbersInList;
+            List<Integer> curIteration = new ArrayList<>();
+            curIteration.add(0);
             for (char operator : start.toCharArray()) {
                 switch (operator) {
                     case ' ':
-                        int t = cur_iteration.remove(cur_iteration.size() - 1);
-                        if (t == 0)
-                            cur_iteration.add(i);
-                        else
-                            cur_iteration.add((int) (t + i * Math.pow(10, String.valueOf(t).length())));
+                        int t = curIteration.remove(curIteration.size() - 1);
+                        if (t == 0) {
+                            curIteration.add(i);
+                        } else {
+                            curIteration.add((int) (t + i * Math.pow(10, String.valueOf(t).length())));
+                        }
                         break;
                     case '+':
-                        cur_iteration.add(i);
+                        curIteration.add(i);
                         break;
                     case '-':
-                        cur_iteration.add(cur_iteration.remove(cur_iteration.size() - 1) * -1);
-                        cur_iteration.add(i);
+                        curIteration.add(curIteration.remove(curIteration.size() - 1) * -1);
+                        curIteration.add(i);
+                        break;
+                    default:
                         break;
                 }
                 i--;
             }
-            if (cur_iteration.stream().mapToInt(Integer::intValue).sum() == 100) {
-                if (cur_iteration.get(0) == 0) cur_iteration.remove(0);
-                Collections.reverse(cur_iteration);
+            if (curIteration.stream().mapToInt(Integer::intValue).sum() == 100) {
+                if (curIteration.get(0) == 0) curIteration.remove(0);
+                Collections.reverse(curIteration);
                 StringBuilder sb = new StringBuilder();
-                for (int elem : cur_iteration) {
+                for (int elem : curIteration) {
                     if (elem > 0 && !String.valueOf(elem).startsWith("1")) sb.append("+");
                     sb.append(elem);
                 }
-                answers.add(sb.toString());
+                if (!answers.contains(sb.toString())) {
+                    answers.add(sb.toString());
+                }
             }
         } else {
             for (char x : c) {
