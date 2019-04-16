33ccc8b6bfa6d4c085513fdbff226f7a8c34895d
Enrico <envomp@ttu.ee>
2019-04-16 11:52:15 +0300

    pr12

---
+++ EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
@@ -0,0 +1,10 @@
+package ee.taltech.iti0202.sum100;
+
+import java.util.List;
+
+public class Sum100 {
+
+    public static List<String> calcSums() {
+        return List.of("100"); // TODO
+    }
+}
