2a63c9563a26bbdb0cb7e7ef7b0c2e0568949297
Enrico <envomp@ttu.ee>
2019-05-08 11:14:00 +0300

    ex14

--- EX13Weather/src/ee/taltech/iti0202/api/strategies/StudentMadeCityFinder.java
+++ EX13Weather/src/ee/taltech/iti0202/api/strategies/StudentMadeCityFinder.java
@@ -2,13 +2,13 @@ package ee.taltech.iti0202.api.strategies;
 
 import ee.taltech.iti0202.api.destinations.City;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 public class StudentMadeCityFinder implements CityFinderStrategy {
     @Override
     public Optional<City> findBestCity(List<City> candidateCities) {
-        return Optional.ofNullable(candidateCities.get(new Random().nextInt(candidateCities.size())));
+        return candidateCities.stream().min(Comparator.comparing(City::getLat));
     }
 }
