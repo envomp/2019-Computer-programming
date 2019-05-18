0017e34a5ecdca7ffd1a347e1a49779928027825
Enrico <envomp@ttu.ee>
2019-05-18 15:40:45 +0300

    ex14

---
+++ PR15Generics/src/ee/taltech/iti0202/generics/foods/Food.java
@@ -0,0 +1,8 @@
+package ee.taltech.iti0202.generics.foods;
+
+public abstract class Food {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ PR15Generics/src/ee/taltech/iti0202/generics/foods/Meat.java
@@ -0,0 +1,12 @@
+package ee.taltech.iti0202.generics.foods;
+
+public class Meat extends Food {
+    public Meat() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "MEAT";
+    }
+}
